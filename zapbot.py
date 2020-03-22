65bba6473d8c87b4cc23c849c94e8448c9f85012
Jhonatan de Souza <jdsjhonatan@hotmail.com>
2020-03-22 08:30:22 -0300

    updating xpath

--- zapbot.py
+++ zapbot.py
@@ -4,21 +4,23 @@ import time
 
 class WhatsappBot:
     def __init__(self):
-        self.mensagem = " Bom dia pessoal, veja o video que acabou de sair https://www.youtube.com"
-        # Altere o nome dos grupos aqui
-        self.grupos = ["GRUPO DA FAMÍLIA", "GRUPO DE VENDAS"]
+        # Parte 1 - A mensagem que você quer enviar
+        self.mensagem = "this is a test"
+        # Parte 2 - Nome dos grupos ou pessoas a quem você deseja enviar a mensagem
+        self.grupos_ou_pessoas = ["GRUPO DA FAMÍLIA", "GRUPO DE VENDAS"]
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
-        self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+        self.driver = webdriver.Chrome(
+            executable_path=r'./chromedriver.exe', chrome_options=options)
 
     def EnviarMensagens(self):
         self.driver.get('https://web.whatsapp.com')
         time.sleep(30)
-        for grupo in self.grupos:
-            grupo = self.driver.find_element_by_xpath(
-                f"//span[@title='{grupo}")
+        for grupo_ou_pessoa in self.grupos_ou_pessoas:
+            campo_grupo = self.driver.find_element_by_xpath(
+                f"//span[@title='{grupo_ou_pessoa}']")
             time.sleep(3)
-            grupo.click()
+            campo_grupo.click()
             chat_box = self.driver.find_element_by_class_name('_13mgZ')
             time.sleep(3)
             chat_box.click()
